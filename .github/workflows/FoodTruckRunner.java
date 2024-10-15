1740eac8c8f551f20918986732edd5eec239576a
SloomyDev <126219030+SloomyDev@users.noreply.github.com>
2024-10-15 08:27:00 -0400

    Create FoodTruckRunner.java

---
+++ .github/workflows/FoodTruckRunner.java
@@ -0,0 +1,21 @@
+public class FoodTruckRunner {
+  public static void main(String[] args) {
+
+    /* ----------------------------------- TO DO -----------------------------------
+     * ✅ Level 6 - Instantiate and print a Dessert object.
+     * -----------------------------------------------------------------------------
+     */
+      Dessert dessert = new Dessert();
+      System.out.println(dessert);
+
+    /* ----------------------------------- TO DO -----------------------------------
+     * ✅ Level 7 - Create an instance of the Dessert subclass then print the object.
+     * -----------------------------------------------------------------------------
+     */
+      Donut donut = new Donut();
+      System.out.println(donut);
+
+
+    
+  }
+}
